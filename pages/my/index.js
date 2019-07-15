540d5e3b51147e76d38ccd306579528f0b3a50fc
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2019-07-15 16:45:20 +0800

    Fix 关于我们

--- pages/my/index.js
+++ pages/my/index.js
@@ -29,7 +29,7 @@ Page({
   aboutUs : function () {
     wx.showModal({
       title: '关于我们',
-      content: '本系统基于开源小程序商城系统 https://github.com/EastWorld/wechat-app-mall 搭建，祝大家使用愉快！',
+      content: 'Oejia 客优云，是一个服务于企业快速业务开发实现的技术服务商，提供优质的企业方案顾问，用于快速、便捷地实现各种基于 Odoo 的业务场景',
       showCancel:false
     })
   },
